74c1cf209407ba35c85bfd792f7ce674e5f6f02c
Matheus Muriel <matheus.muriel@outlook.com>
2021-04-19 11:44:40 -0300

    Criado o Welcomer

---
+++ lib/inmana/welcomer.ex
@@ -0,0 +1,22 @@
+defmodule Inmana.Welcomer do
+  def welcome(%{"name" => name, "age" => age}) do
+    age = String.to_integer(age)
+
+    name
+    |> String.trim()
+    |> String.downcase()
+    |> evaluete(age)
+  end
+
+  defp evaluete("Banana", 42) do
+    {:ok, "You are very special Sr. Banana."}
+  end
+
+  defp evaluete(name, age) when age >= 18 do
+    {:ok, "Welcome #{name}."}
+  end
+
+  defp evaluete(name, _age) do
+    {:error, "You shall not pass #{name}"}
+  end
+end
