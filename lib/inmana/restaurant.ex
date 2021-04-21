4808395444b641a4ac1ab3ed16b87a06aab2fdfc
Matheus Muriel <matheus.muriel@outlook.com>
2021-04-21 13:49:52 -0300

    Restaurant View

--- lib/inmana/restaurant.ex
+++ lib/inmana/restaurant.ex
@@ -4,6 +4,11 @@ defmodule Inmana.Restaurant do
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  # @ Define uma Variavel de Modulo
+  @required_params [:email, :name]
+
+  @derive {Jason.Encoder, only: @required_params ++ [:id]}
+
   schema "restaurants" do
     field :email, :string
     field :name, :string
@@ -14,8 +19,8 @@ defmodule Inmana.Restaurant do
   def changeset(params) do
     # Criando uma "Structure" do modulo
     %__MODULE__{}
-    |> cast(params, [:email, :name])
-    |> validate_required([:email, :name])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
     |> validate_length(:name, min: 2)
     # Regex usando Sigils
     |> validate_format(:email, ~r/@/)
---
+++ lib/inmana_web/controllers/restaurants_controller.ex
@@ -0,0 +1,19 @@
+defmodule InmanaWeb.RestaurantsController do
+  use InmanaWeb, :controller
+
+  alias Inmana.Restaurant
+  alias Inmana.Restaurants.Create
+
+  alias InmanaWeb.FallbackController
+
+  action_fallback FallbackController
+
+  def create(conn, params) do
+    # Estrutura de controle de Match Patern
+    with {:ok, %Restaurant{} = restaurant} <- Create.call(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", restaurant: restaurant)
+    end
+  end
+end
